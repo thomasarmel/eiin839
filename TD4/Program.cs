79b39766e5dfd776d5a5cbcea190d8a55e6ea349
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2022-03-01 14:42:15 +0100

    Add TD4

---
+++ TD4/Program.cs
@@ -0,0 +1,4 @@
+﻿using ServiceReference1;
+
+var client = new ServiceReference1.CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+Console.WriteLine(await client.ChannelFactory.CreateChannel().AddAsync(2, 5));
