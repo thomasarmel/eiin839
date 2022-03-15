96a1255b9a2ea1db36e70ece349758cac3bab0ab
Thomas Prévost <7011145@gmail.com>
2022-03-15 14:12:42 +0100

    Add TD6

---
+++ TD6/MathsClient/Program.cs
@@ -0,0 +1,4 @@
+﻿using ServiceReference1;
+
+var client = new ServiceReference1.MathsOperationsClient(MathsOperationsClient.EndpointConfiguration.BasicHttpBinding_IMathsOperations);
+Console.WriteLine(client.Multiply(2, 3));
---
+++ TD6/MathsLibrary/MathsOperations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MathsLibrary
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
+    public class MathsOperations : IMathsOperations
+    {
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        int IMathsOperations.Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        CompositeType IMathsOperations.GetDataUsingDataContract(CompositeType composite)
+        {
+            throw new NotImplementedException();
+        }
+
+        int IMathsOperations.Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        int IMathsOperations.Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
