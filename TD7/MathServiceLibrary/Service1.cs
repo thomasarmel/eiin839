5bbc6e1c5bb3eb4c5768b79ca629b9871d29315d
Thomas Prévost <7011145@gmail.com>
2022-03-29 17:58:54 +0200

    Can listen on multiple ports, mutlipe URIs and wsHttpBinding

---
+++ TD7/MathServiceLibrary/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MathServiceLibrary
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
+    public class Service1 : IService1
+    {
+        int IService1.add(int a, int b)
+        {
+            return a + b;
+        }
+
+        int IService1.div(int a, int b)
+        {
+            return a / b;
+        }
+
+        int IService1.mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        int IService1.sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ TD7/Program.cs
@@ -0,0 +1,4 @@
+﻿using MathServiceReference;
+
+var client = new MathServiceReference.Service1Client(Service1Client.EndpointConfiguration.BasicHttpBinding_IService1);
+Console.WriteLine(client.ChannelFactory.CreateChannel().add(2, 5));
